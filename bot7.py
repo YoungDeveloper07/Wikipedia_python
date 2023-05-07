395b9b8fb0fa155fdd2b2ddd83b83d069323f477
YoungDeveloper07 <125660368+YoungDeveloper07@users.noreply.github.com>
2023-05-07 16:41:40 +0500

    Update bot7.py

--- bot7.py
+++ bot7.py
@@ -11,11 +11,19 @@ logging.basicConfig(level=logging.INFO)
 bot = Bot(token=bot_token)
 
 dp=Dispatcher(bot)
-
+keyboard1 = types.ReplyKeyboardMarkup(row_width=2, resize_keyboard=True)
+keyboard1.add( types.KeyboardButton(text='📚Bot haqida'))
 @dp.message_handler(commands=['start'])
 async def salom(message: types.Message):
     await message.answer('Salom.Botga xush kelibsiz. Nima haqida bilmoqchisiz?')
-
+    
+@dp.message_handler(text='📚Bot haqida')
+async def send_bothaqaida(message: types.Message):
+    await message.answer(
+        text=f"👨🏻‍💻 Loyiha asoschisi — @young_developer_07 va @Ilyosbek_Kv\n"
+           
+             f"📩 Murojaatlar uchun — @young_developer_07"
+    )
 @dp.message_handler()
 async def wiki(message: types.Message):
     try: 
